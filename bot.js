c442979250e80fc704e81134f8c29c810613100c
SquadXiii <39181388+SquadXiii@users.noreply.github.com>
2018-05-11 07:47:11 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,32 @@
+{
+  "ClientId": 444028417265106954,
+  "Token": "NDQ0MDI4NDE3MjY1MTA2OTU0.DdWDsw.gjWiEccLeLUXqNUM__qrgynwsVg",
+  "OwnerIds": [
+    214299631352283137
+    
+  ],
+  "LoLApiKey": "",
+  "GoogleApiKey": "AIzaSyCxnmkAt0VCttJiRKX20otXoxg9oXdFR5A",
+  "MashapeKey": "",
+  "OsuApiKey": "",
+  "SoundCloudClientId": "",
+  "CleverbotApiKey": "",
+  "CarbonKey": "",
+  "Db": {
+    "Type": "sqlite",
+    "ConnectionString": "Data Source=data/NadekoBot.db"
+  },
+  "TotalShards": 1,
+  "PatreonAccessToken": "",
+  "PatreonCampaignId": "334038",
+  "RestartCommand": null,
+  "ShardRunCommand": "",
+  "ShardRunArguments": "",
+  "ShardRunPort": null,
+  "MiningProxyUrl": null,
+  "MiningProxyCreds": null,
+  "BotListToken": null,
+  "TwitchClientId": null,
+  "VotesToken": null,
+  "VotesUrl": null
+}
